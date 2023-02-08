c42c12cc4d7fa8cee4431190fc236605874fa323
Osama <107386980+osSidd@users.noreply.github.com>
2023-02-08 14:22:29 +0530

    modified auth

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -4,19 +4,21 @@ const router = express.Router();
 const blogs = require('../controllers/blogsController');
 const authRoutes = require('../middleware/authRoutes')
 
-//authorizing users
-router.use(authRoutes)
-
 //routes
 
 //get all blogs
 router.get('/', blogs.get_all_blogs);
 
-//post a new blog
-router.post('/', blogs.post_new_blog);
-
 //get all comments
 router.get('/:id/comments', blogs.get_all_comments)
+
+
+//authorizing users
+router.use(authRoutes)
+
+
+//post a new blog
+router.post('/', blogs.post_new_blog);
 
 //post a new comment for a specific blog
 router.post('/:id/comments', blogs.post_new_comment);
